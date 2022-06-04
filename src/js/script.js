26651d88540713dd1848eb41ca5d5532f7bab578
iMax_3D <padalkamaksym@gmail.com>
2022-06-05 00:23:23 +0300

    created tabs section

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,13 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         adaptiveHeight: false,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>'
-//       });
-//   });
-
-var slider = tns({
+const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
-    autoplay: false,
+    autoplay: true,
     speed: 1200,
     nav: false,
     controls: false,
     navPosition: 'bottom',
+    autoplayButtonOutput: false,
     responsive: {
         320: {
             nav: true
@@ -41,4 +31,26 @@ var slider = tns({
   document.querySelector('.next').onclick = function () {
     slider.goTo('next');
   };
+
+
+$(document).ready(function(){
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+    
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
 
